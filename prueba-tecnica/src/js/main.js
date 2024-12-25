c1e49a39f39fbcd195d6f683dfd6ae3104664bcf
Exequiel Soler <exesoler@gmail.com>
2024-12-25 17:14:23 -0300

    Listo modal Upload Listo Funcionalidad de Fullscreen Step 2 v0.9

--- prueba-tecnica/src/js/main.js
+++ prueba-tecnica/src/js/main.js
@@ -1,5 +1,9 @@
 const tableRows = document.querySelectorAll('tbody tr:not(:last-child)');
 const modal = document.querySelector('.uploadModal');
+const dragndropContainer = document.querySelector('.uploadModalBody__dragAndDrop');
+const progressContainer = document.querySelector('.uploadModalBody__progress');
+const prograssBar = document.querySelector('.progress__bar');
+const body = document.querySelector('body');
 
 // when mouse over, change class only on that row
 tableRows.forEach(row => {
@@ -15,10 +19,40 @@ window.onload = function() {
     const input = document.querySelector('.edit input:first-child').focus();
 }
 
-const openModal = () => {
-    modal.classList.remove('hidden');
-};
+const openModal = () => modal.classList.remove('hidden');
 
 const closeModal = () => {
     modal.classList.add('hidden');
+    resetUpload();
+};
+
+const changeUpload = () => {
+    dragndropContainer.classList.add('hidden');
+    progressContainer.classList.remove('hidden');
+    setTimeout(() => {
+        prograssBar.style.width = '100%';
+        setTimeout(() => {
+            progressContainer.classList.add('uploadModalBody__progress--success');
+            setTimeout(() => {
+                closeModal();
+            }, 2000);
+        }, 3000);
+    }, 150);
+
+};
+
+const resetUpload = () => {
+    progressContainer.classList.add('hidden');
+    dragndropContainer.classList.remove('hidden');
+    progressContainer.classList.remove('uploadModalBody__progress--success');
+    prograssBar.style.width = '0%';
+};
+
+
+const openFullScreen = () => {
+    body.classList.add('tableFullScreen');
+};
+
+const closeFullScreen = () => {
+    body.classList.remove('tableFullScreen');
 };
